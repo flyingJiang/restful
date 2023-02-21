66294bbed11fd5b8f5226bf1a8856269c614dac4
jiangjianfei <jiangjianfei@baidu.com>
2023-02-21 21:44:06 +0800

    测试curl

---
+++ src/main/resources/curl/api_put.sh
@@ -0,0 +1,6 @@
+curl --location --request PUT 'http://localhost:8981/v1/func1/users/zhang1' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "张1234",
+    "username": "zhang1"
+}'
